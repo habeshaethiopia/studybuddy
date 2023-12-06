9a8d40787c7262a3a6edfadf384a8e70da43a5d4
Adane <adanemoges6@gmail.com>
2023-12-06 04:29:34 +0300

    integgrating the auth0

--- app.py
+++ app.py
@@ -16,20 +16,45 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma
+from functools import lru_cache
 import constants
-import secrets
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
 
-def generate_secret_key():
-    return secrets.token_hex(16)
-os.environ["OPENAI_API_KEY"] = constants.APIKEY
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
 app = Flask(__name__)
-app.secret_key = generate_secret_key()
+app.secret_key = env.get("APP_SECRET_KEY")
+
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
+# app.py
+
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
 UPLOAD_FOLDER = "static/data"
 ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# Enable to save to disk & reuse the model (for repeated queries on the same data)
+"""Enable to save to disk & reuse the model
+(for repeated queries on the same data)"""
 PERSIST = True
 
 
@@ -37,9 +62,15 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@lru_cache(maxsize=None)
 def initialize_chat_chain(fname):
-    loader = UnstructuredPDFLoader("static/"+fname)
-
+    loader = UnstructuredPDFLoader("static/" + fname)
+    if PERSIST and os.path.exists("persist"):
+        print("Reusing index...\n")
+        vectorstore = Chroma(
+            persist_directory="persist", embedding_function=OpenAIEmbeddings()
+        )
+        index = VectorStoreIndexWrapper(vectorstore=vectorstore)
     if PERSIST:
         index = VectorstoreIndexCreator(
             vectorstore_kwargs={"persist_directory": "persist"}
@@ -53,26 +84,26 @@ def initialize_chat_chain(fname):
     )
     return chain
 
-@app.route("/")
-def home():
+
+@app.route("/index")
+def home2():
     return render_template("index.html")
 
 
 @app.route("/chat/<filename>", methods=["GET", "POST"])
 def chat(filename):
-
     filename = UPLOAD_FOLDER.replace("static/", "") + "/" + filename
 
     chain = initialize_chat_chain(filename)
     if request.method == "POST":
         message = request.form["message"]
-         # Get the chat history from the session
-        chat_history = session.get('chat_history', [])
+        # Get the chat history from the session
+        chat_history = session.get("chat_history", [])
         # Store the updated chat history in the session
         result = chain({"question": message, "chat_history": chat_history})
         answer = result["answer"]
         chat_history.append((message, answer))
-        session['chat_history'] = chat_history
+        session["chat_history"] = chat_history
 
         return jsonify({"answer": answer})
 
@@ -100,6 +131,46 @@ def upload_file():
     return render_template("index.html")
 
 
+# Controllers API
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        session=session.get("user"),
+        pretty=json.dumps(session.get("user"), indent=4),
+    )
+
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+
 if __name__ == "__main__":
-    app.run(port=5050,debug=True)
-# analyze it if it has server error
+    app.run(host="0.0.0.0", port=env.get("PORT", 3000), debug=True)
